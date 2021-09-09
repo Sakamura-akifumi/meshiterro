2bb3ab0a7c7755d963471f370cc4b2f1a44b264d
Sakamura-akifumi <tmr20th08moon@gmail.com>
2021-09-09 02:15:49 +0000

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'd99fa173d54cf06c412ba32575c8f46347cdb8497d6b6404f913f368614c2cecf133723cb99e9f3b9dbf5e7aedf31688ec77e2ad3b98e5da053801982b09f64b'
